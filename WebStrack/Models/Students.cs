49dd88e36cbeb670d3e2305dfc579d84156690b6
Axel M <Axel_mendez1@hotmail.com>
2019-11-19 18:49:43 -0300

    Proyecto completo

---
+++ WebStrack/Models/Students.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebStrack.Models
+{
+    public partial class Students
+    {
+        public int StudentId { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public string Pais { get; set; }
+    }
+}
---
+++ WebStrack/Models/TESTINGContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebStrack.Models
+{
+    public partial class TESTINGContext : DbContext
+    {
+        public TESTINGContext()
+        {
+        }
+
+        public TESTINGContext(DbContextOptions<TESTINGContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Students> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=TESTING;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Students>(entity =>
+            {
+                entity.HasKey(e => e.StudentId);
+
+                entity.Property(e => e.Email).HasMaxLength(200);
+
+                entity.Property(e => e.Nombre).HasMaxLength(50);
+
+                entity.Property(e => e.Pais).HasMaxLength(50);
+
+                entity.Property(e => e.Telefono).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
